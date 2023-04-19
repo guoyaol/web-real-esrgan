919fa466e1a1d831e0a2bb4b398f5c66328ccb03
guoyaol <guoyaoli00@gmail.com>
2023-04-19 12:22:58 -0400

    jpg pipeline finished

--- pipeline.py
+++ pipeline.py
@@ -7,132 +7,6 @@ from torch.nn import functional as F
 from network import RRDBNet
 
 
-class RealESRGANer():
-
-    def __init__(self,
-                 scale,
-                 model_path,
-                 model=None,
-                 device=None):
-        self.scale = scale
-        self.mod_scale = None
-
-        # initialize model
-
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') if device is None else device
-
-        loadnet = torch.load(model_path, map_location=torch.device('cpu'))
-
-
-        model.load_state_dict(loadnet['params_ema'], strict=True)
-
-        model.eval()
-        self.model = model.to(self.device)
-
-
-    def pre_process(self, img):
-        """Pre-process, such as pre-pad and mod pad, so that the images can be divisible
-        """
-        img = torch.from_numpy(np.transpose(img, (2, 0, 1))).float()
-        self.img = img.unsqueeze(0).to(self.device)
-
-
-        # mod pad for divisible borders
-        if self.scale == 2:
-            self.mod_scale = 2
-        elif self.scale == 1:
-            self.mod_scale = 4
-        if self.mod_scale is not None:
-            self.mod_pad_h, self.mod_pad_w = 0, 0
-            _, _, h, w = self.img.size()
-            if (h % self.mod_scale != 0):
-                self.mod_pad_h = (self.mod_scale - h % self.mod_scale)
-            if (w % self.mod_scale != 0):
-                self.mod_pad_w = (self.mod_scale - w % self.mod_scale)
-            self.img = F.pad(self.img, (0, self.mod_pad_w, 0, self.mod_pad_h), 'reflect')
-
-    def process(self):
-        # model inference
-        self.output = self.model(self.img)
-
-    def post_process(self):
-        # remove extra pad
-        if self.mod_scale is not None:
-            _, _, h, w = self.output.size()
-            self.output = self.output[:, :, 0:h - self.mod_pad_h * self.scale, 0:w - self.mod_pad_w * self.scale]
-        # remove prepad
-        return self.output
-
-    @torch.no_grad()
-    def enhance(self, img, outscale=None, alpha_upsampler='realesrgan'):
-        h_input, w_input = img.shape[0:2]
-        # img: numpy
-        img = img.astype(np.float32)
-        if np.max(img) > 256:  # 16-bit image
-            max_range = 65535
-            print('\tInput is a 16-bit image')
-        else:
-            max_range = 255
-        img = img / max_range
-        if len(img.shape) == 2:  # gray image
-            img_mode = 'L'
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        elif img.shape[2] == 4:  # RGBA image with alpha channel
-            img_mode = 'RGBA'
-            alpha = img[:, :, 3]
-            img = img[:, :, 0:3]
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            if alpha_upsampler == 'realesrgan':
-                alpha = cv2.cvtColor(alpha, cv2.COLOR_GRAY2RGB)
-        else:
-            img_mode = 'RGB'
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        # ------------------- process image (without the alpha channel) ------------------- #
-        self.pre_process(img)
-
-        self.process()
-
-        output_img = self.post_process()
-        output_img = output_img.data.squeeze().float().cpu().clamp_(0, 1).numpy()
-        output_img = np.transpose(output_img[[2, 1, 0], :, :], (1, 2, 0))
-        if img_mode == 'L':
-            output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2GRAY)
-
-        # ------------------- process the alpha channel if necessary ------------------- #
-        if img_mode == 'RGBA':
-            if alpha_upsampler == 'realesrgan':
-                self.pre_process(alpha)
-
-
-                self.process()
-
-                output_alpha = self.post_process()
-                output_alpha = output_alpha.data.squeeze().float().cpu().clamp_(0, 1).numpy()
-                output_alpha = np.transpose(output_alpha[[2, 1, 0], :, :], (1, 2, 0))
-                output_alpha = cv2.cvtColor(output_alpha, cv2.COLOR_BGR2GRAY)
-            else:  # use the cv2 resize for alpha channel
-                h, w = alpha.shape[0:2]
-                output_alpha = cv2.resize(alpha, (w * self.scale, h * self.scale), interpolation=cv2.INTER_LINEAR)
-
-            # merge the alpha channel
-            output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2BGRA)
-            output_img[:, :, 3] = output_alpha
-
-        # ------------------------------ return ------------------------------ #
-        if max_range == 65535:  # 16-bit image
-            output = (output_img * 65535.0).round().astype(np.uint16)
-        else:
-            output = (output_img * 255.0).round().astype(np.uint8)
-
-        if outscale is not None and outscale != float(self.scale):
-            output = cv2.resize(
-                output, (
-                    int(w_input * outscale),
-                    int(h_input * outscale),
-                ), interpolation=cv2.INTER_LANCZOS4)
-
-        return output, img_mode
 
 netscale = 4
 model_path = "./weights/RealESRGAN_x4plus.pth"
@@ -141,13 +15,8 @@ outscale = 4
 
 
 
-upsampler = RealESRGANer(
-    scale=netscale,
-    model_path=model_path,
-    model=model)
 
-
-input_path = "./input/OST_009.png"
+input_path = "./input/0014.jpg"
 output_path = "./output"
 
 imgname, extension = os.path.splitext(os.path.basename(input_path))
@@ -160,18 +29,94 @@ else:
     img_mode = None
 
 
+#load model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+loadnet = torch.load(model_path, map_location=torch.device('cpu'))
+model.load_state_dict(loadnet['params_ema'], strict=True)
+model.to(device)
+model.eval()
 
 
-output, _ = upsampler.enhance(img, outscale=outscale)
+alpha_upsampler='realesrgan'
+h_input, w_input = img.shape[0:2]
+# img: numpy
+img = img.astype(np.float32)
+if np.max(img) > 256:  # 16-bit image
+    max_range = 65535
+    print('\tInput is a 16-bit image')
+else:
+    max_range = 255
+img = img / max_range
+if len(img.shape) == 2:  # gray image
+    img_mode = 'L'
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+elif img.shape[2] == 4:  # RGBA image with alpha channel
+    img_mode = 'RGBA'
+    alpha = img[:, :, 3]
+    img = img[:, :, 0:3]
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    if alpha_upsampler == 'realesrgan':
+        alpha = cv2.cvtColor(alpha, cv2.COLOR_GRAY2RGB)
+else:
+    img_mode = 'RGB'
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#preprocess image
+def preprocess(img, device):
+    img = torch.from_numpy(np.transpose(img, (2, 0, 1))).float()
+    img = img.unsqueeze(0).to(device)
+    return img
+
+img = preprocess(img, device)
+
+#model inference
+output_img = model(img)
+
+
+#post process
+
+output_img = output_img.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+output_img = np.transpose(output_img[[2, 1, 0], :, :], (1, 2, 0))
+if img_mode == 'L':
+    output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2GRAY)
+
+# ------------------- process the alpha channel if necessary ------------------- #
+if img_mode == 'RGBA':
+    if alpha_upsampler == 'realesrgan':
+        alpha = preprocess(alpha, device)
+
+        output_alpha = model(alpha)
+
+        output_alpha = output_alpha.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        output_alpha = np.transpose(output_alpha[[2, 1, 0], :, :], (1, 2, 0))
+        output_alpha = cv2.cvtColor(output_alpha, cv2.COLOR_BGR2GRAY)
+
+    # merge the alpha channel
+    output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2BGRA)
+    output_img[:, :, 3] = output_alpha
+
+# ------------------------------ return ------------------------------ #
+if max_range == 65535:  # 16-bit image
+    output = (output_img * 65535.0).round().astype(np.uint16)
+else:
+    output = (output_img * 255.0).round().astype(np.uint8)
+
+if outscale is not None and outscale != float(netscale):
+    output = cv2.resize(
+        output, (
+            int(w_input * outscale),
+            int(h_input * outscale),
+        ), interpolation=cv2.INTER_LANCZOS4)
 
 
 
 
+#save the output image
 extension = extension[1:]
 
 if img_mode == 'RGBA':  # RGBA images should be saved in png format
     extension = 'png'
-
 
 save_path = os.path.join(output_path, f'{imgname}.{extension}')
 
