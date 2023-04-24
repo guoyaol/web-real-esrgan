eea129d97ef28c548a6a2807263a7a7ed3418ebf
guoyaol <guoyaoli00@gmail.com>
2023-04-24 14:40:38 -0400

    implemented preprocess

---
+++ modules/preprocess.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+import os
+import torch
+import math
+from torch.nn import functional as F
+import tvm
+from tvm import relax
+from tvm.script import relax as R
+from tvm.relax.frontend.torch import dynamo_capture_subgraphs
+import torch
+
+
+def preprocess() -> tvm.IRModule:
+    from tvm import te
+    #np.transpose(img, (2, 0, 1)) and unqueeze(0)
+    #todo: different sizes of images
+    def f_preprocess(A):
+        def fcompute(i, c, x, y):
+            return A[x, y, c]
+        return te.compute((1, 3, 640, 448), fcompute, name="preprocess")
+
+
+    bb = relax.BlockBuilder()
+    x = relax.Var("x", R.Tensor([640, 448, 3], "float32"))
+    with bb.function("preprocess", [x]):
+        image = bb.emit(
+            bb.call_te(f_preprocess, x, primfunc_name_hint="tir_preprocess")
+        )
+        bb.emit_func_output(image)
+    return bb.get()
+
+input_path = "/home/guoyaol/web-real-esrgan/input/OST_009.png"
+
+imgname, extension = os.path.splitext(os.path.basename(input_path))
+img = cv2.imread(input_path, cv2.IMREAD_UNCHANGED)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+img = img.astype(np.float32)
+max_range = 255
+img = img / max_range
+
+#our result
+print("our result")
+img_nd = tvm.nd.array(img.reshape(640, 448, 3).astype("float32"))
+
+p_mod = preprocess()
+
+ex = relax.build(p_mod, target= "llvm")
+vm = relax.VirtualMachine(ex, tvm.cpu())
+nd_res1 = vm["preprocess"](img_nd)
+
+print(nd_res1)
+print(nd_res1.shape)
+
+
+#ref result
+print("ref result")
+img = torch.from_numpy(np.transpose(img, (2, 0, 1))).float()
+img = img.unsqueeze(0)
+print(img)
+print(img.shape)
+
--- modules/scale.py
+++ modules/scale.py
@@ -16,12 +16,12 @@ def scale_image() -> tvm.IRModule:
     #todo: different sizes of images
     def f_scale_image(A):
         def fcompute(y, x, c):
-            return A[0, y, x, c] / 255
+            return A[y, x, c] / 255
 
-        return te.compute((1, 640, 448, 3), fcompute, name="scale_image")
+        return te.compute((640, 448, 3), fcompute, name="scale_image")
 
     bb = relax.BlockBuilder()
-    x = relax.Var("x", R.Tensor([1, 640, 448, 3], "float32"))
+    x = relax.Var("x", R.Tensor([640, 448, 3], "float32"))
     with bb.function("scale_image", [x]):
         image = bb.emit(
             bb.call_te(f_scale_image, x, primfunc_name_hint="tir_scale_image")
@@ -36,17 +36,21 @@ imgname, extension = os.path.splitext(os.path.basename(input_path))
 img = cv2.imread(input_path, cv2.IMREAD_UNCHANGED)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-print(img)
-print(img.shape)
 
-img_nd = tvm.nd.array(img.reshape(1, 640, 448, 3).astype("float32"))
+# our result
+img_nd = tvm.nd.array(img.astype("float32"))
 
 s_mod = scale_image()
-s_mod.show()
-
 
 ex = relax.build(s_mod, target= "llvm")
 vm = relax.VirtualMachine(ex, tvm.cpu())
-nd_res = vm["scale_image"](img_nd)
+nd_res1 = vm["scale_image"](img_nd)
 
-print(nd_res)
+print(nd_res1)
+print(type(nd_res1))
+
+# ref result
+img = img.astype(np.float32)
+max_range = 255
+img = img / max_range
+print(img)
