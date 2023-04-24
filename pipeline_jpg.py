f493c1911590313f12475edb85493d20a1db3393
guoyaol <guoyaoli00@gmail.com>
2023-04-23 20:34:47 -0400

    no image model

--- pipeline_jpg.py
+++ pipeline_jpg.py
@@ -54,10 +54,8 @@ imgname, extension = os.path.splitext(os.path.basename(input_path))
 
 img = cv2.imread(input_path, cv2.IMREAD_UNCHANGED)
 
-if len(img.shape) == 3 and img.shape[2] == 4:
-    img_mode = 'RGBA'
-else:
-    img_mode = None
+
+img_mode = None
 
 
 #1. load model
@@ -82,19 +80,8 @@ with torch.no_grad():
     img = img / max_range
 
     #3. convert image to RGB, color spcace
-    if len(img.shape) == 2:  # gray image
-        img_mode = 'L'
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    elif img.shape[2] == 4:  # RGBA image with alpha channel
-        img_mode = 'RGBA'
-        alpha = img[:, :, 3]
-        img = img[:, :, 0:3]
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        if alpha_upsampler == 'realesrgan':
-            alpha = cv2.cvtColor(alpha, cv2.COLOR_GRAY2RGB)
-    else:
-        img_mode = 'RGB'
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_mode = 'RGB'
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     #4. preprocess image
     def preprocess(img, device):
@@ -111,23 +98,7 @@ with torch.no_grad():
     #6. post process
     output_img = output_img.data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output_img = np.transpose(output_img[[2, 1, 0], :, :], (1, 2, 0))
-    if img_mode == 'L':
-        output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2GRAY)
 
-    #7. process the alpha channel if necessary ------------------- #
-    if img_mode == 'RGBA':
-        if alpha_upsampler == 'realesrgan':
-            alpha = preprocess(alpha, device)
-
-            output_alpha = model(alpha)
-
-            output_alpha = output_alpha.data.squeeze().float().cpu().clamp_(0, 1).numpy()
-            output_alpha = np.transpose(output_alpha[[2, 1, 0], :, :], (1, 2, 0))
-            output_alpha = cv2.cvtColor(output_alpha, cv2.COLOR_BGR2GRAY)
-
-        # merge the alpha channel
-        output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2BGRA)
-        output_img[:, :, 3] = output_alpha
 
     #8. un-scale image
     if max_range == 65535:  # 16-bit image
@@ -138,9 +109,6 @@ with torch.no_grad():
 
     #9. save the output image
     extension = extension[1:]
-
-    if img_mode == 'RGBA':  # RGBA images should be saved in png format
-        extension = 'png'
 
     save_path = os.path.join(output_path, f'{imgname}.{extension}')
 
