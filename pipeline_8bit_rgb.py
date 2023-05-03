b91f86495654623bb53231261a8e925a89d8943f
guoyaol <guoyaoli00@gmail.com>
2023-05-03 18:23:05 -0400

    update pipeline

--- pipeline_8bit_rgb.py
+++ pipeline_8bit_rgb.py
@@ -58,8 +58,8 @@ def scale_image() -> tvm.IRModule:
     #divide each element by 255
     #todo: different sizes of images
     def f_scale_image(A):
-        def fcompute(y, x, c):
-            return A[y, x, c] / 255
+        def fcompute(x, y, c):
+            return A[x, y, c] / te.const(255, "float32")
 
         return te.compute((640, 448, 3), fcompute, name="scale_image")
 
@@ -108,6 +108,52 @@ def unscale_image() -> tvm.IRModule:
             bb.call_te(f_unscale_image, x, primfunc_name_hint="tir_unscale_image")
         )
         bb.emit_func_output(image)
+    return bb.get()
+
+def postprocess() -> tvm.IRModule:
+    from tvm import te
+    # output_img = output_img.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+    # output_img = np.transpose(output_img[[2, 1, 0], :, :], (1, 2, 0))
+    def f_squeeze(A):
+        def fcompute(c, x, y):
+            return A[0, c, x, y]
+        return te.compute((3, 2560, 1792), fcompute, name="squeeze")
+
+    def f_swapchannel(A):
+        def fcompute(c, x, y):
+            return A[2-c, x, y]
+        return te.compute((3, 2560, 1792), fcompute, name="swapnnel")
+    
+    def f_transpose(A):
+        def fcompute(x, y, c):
+            return A[c, x, y]
+        return te.compute((2560, 1792, 3), fcompute, name="transpose")
+    
+    def f_max_0(A):
+        def fcompute(c, x, y):
+            return te.if_then_else(A[c, x, y] > te.const(0, "float32"), A[c, x, y], te.const(0, "float32"))
+        return te.compute((3, 2560, 1792), fcompute, name="max0")
+    
+    def f_min_1(A):
+        def fcompute(c, x, y):
+            return te.if_then_else(A[c, x, y] < te.const(1, "float32"), A[c, x, y], te.const(1, "float32"))
+        return te.compute((3, 2560, 1792), fcompute, name="min1")
+
+
+    bb = relax.BlockBuilder()
+    x = relax.Var("x", R.Tensor([1, 3, 2560, 1792], "float32"))
+    with bb.function("postprocess", [x]):
+        #squeeze
+        squeezed = bb.emit(bb.call_te(f_squeeze, x, primfunc_name_hint="tir_squeeze"))
+        #clamp
+        maxed = bb.emit(bb.call_te(f_max_0, squeezed, primfunc_name_hint="tir_max_0"))
+        clamped = bb.emit(bb.call_te(f_min_1, maxed, primfunc_name_hint="tir_min_1"))
+        #rgb swap
+        swapped = bb.emit(bb.call_te(f_swapchannel, clamped, primfunc_name_hint="tir_swapchannel"))
+        #transpose
+        out_image = bb.emit(bb.call_te(f_transpose, swapped, primfunc_name_hint="tir_transpose"))
+
+        bb.emit_func_output(out_image)
     return bb.get()
 
 #1. scale image
