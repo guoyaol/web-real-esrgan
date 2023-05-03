fef5a964658de3d853a93c96040f3410c2205a27
guoyaol <guoyaoli00@gmail.com>
2023-05-03 16:52:47 -0400

    first version of post process

--- modules/postprocess.py
+++ modules/postprocess.py
@@ -15,27 +15,30 @@ def postprocess() -> tvm.IRModule:
     from tvm import te
     # output_img = output_img.data.squeeze().float().cpu().clamp_(0, 1).numpy()
     # output_img = np.transpose(output_img[[2, 1, 0], :, :], (1, 2, 0))
-    def f_postprocess(A):
+    def f_swapchannel(A):
+        def fcompute(c, x, y):
+            return A[2-c, x, y]
+        return te.compute((3, 2560, 1792), fcompute, name="swapnnel")
+    
+    def f_transpose(A):
         def fcompute(x, y, c):
-            # suqeeze
-
-            #clamp
-
-            # rgb switch
-
-            #transpose
-
-
-            
-        return te.compute((2560, 1792, 3), fcompute, name="postprocess")
+            return A[c, y, x]
+        return te.compute((2560, 1792, 3), fcompute, name="transpose")
 
 
     bb = relax.BlockBuilder()
     x = relax.Var("x", R.Tensor([1, 3, 2560, 1792], "float32"))
     with bb.function("postprocess", [x]):
-        image = bb.emit(
-            bb.call_te(f_postprocess, x, primfunc_name_hint="tir_postprocess")
-        )
-        bb.emit_func_output(image)
+        #squeeze
+        squeezed = bb.emit(relax.op.squeeze(x))
+        #clamp
+        maxed = bb.emit(relax.op.maximum(squeezed, relax.op.const(0, "float32")))
+        clamped = bb.emit(relax.op.minimum(maxed, relax.op.const(1, "float32")))
+        #rgb swap
+        swapped = bb.emit(bb.call_te(f_swapchannel, clamped, primfunc_name_hint="tir_swapchannel"))
+        #transpose
+        out_image = bb.emit(bb.call_te(f_transpose, swapped, primfunc_name_hint="tir_transpose"))
+
+        bb.emit_func_output(out_image)
     return bb.get()
 
