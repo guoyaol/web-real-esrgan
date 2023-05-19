8fd99e0de91b17a97f302c1df6efdb0227821cfa
guoyaol <guoyaoli00@gmail.com>
2023-05-19 17:37:41 -0400

    add evaluation

---
+++ eval/eval_torch.py
@@ -0,0 +1,70 @@
+import numpy as np
+import cv2
+import os
+import torch
+import math
+from torch.nn import functional as F
+from network import RRDBNet
+import tvm
+from tvm import relax
+from tvm.script import relax as R
+from tvm.relax.frontend.torch import dynamo_capture_subgraphs
+import torch
+from typing import Dict, List, Tuple
+import time
+
+
+
+input_path = "/Users/guoyaoli/tvm_work/web-real-esrgan/input/OST_009.png"
+output_path = "./output"
+
+imgname, extension = os.path.splitext(os.path.basename(input_path))
+img = cv2.imread(input_path, cv2.IMREAD_UNCHANGED)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+netscale = 4
+model_path = "/Users/guoyaoli/tvm_work/web-real-esrgan/weights/RealESRGAN_x4plus.pth"
+model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+loadnet = torch.load(model_path, map_location=torch.device('cpu'))
+model.load_state_dict(loadnet['params_ema'], strict=True)
+outscale = 4
+
+#1. scale image
+img = img.astype(np.float32)
+max_range = 255
+img = img / max_range
+
+
+#2. preprocess image
+img = torch.from_numpy(np.transpose(img, (2, 0, 1))).float()
+img = img.unsqueeze(0)
+
+
+# 3. model inference
+
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('mps')
+model.to(device)
+model.eval()
+
+with torch.no_grad():
+    img = img.to(device)
+    output_img = model(img)
+
+
+#4. post process
+output_img = output_img.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+output_img = np.transpose(output_img[[2, 1, 0], :, :], (1, 2, 0))
+
+
+#5. re-scale image
+output = (output_img * 255.0).round().astype(np.uint8)
+
+
+#---------------------save image---------------------
+extension = extension[1:]
+
+save_path = os.path.join(output_path, f'{imgname}.{extension}')
+
+cv2.imwrite(save_path, output)
---
+++ eval/eval_tvm.py
@@ -0,0 +1,79 @@
+import tvm
+from tvm import relax
+import cv2
+import numpy as np
+import os
+from typing import Dict, List
+
+target = tvm.target.Target("apple/m2-gpu")
+device = tvm.metal()
+
+def load_params(artifact_path: str, device) -> Dict[str, List[tvm.nd.NDArray]]:
+    from tvm.contrib import tvmjs
+
+    pdict = {}
+    params, meta = tvmjs.load_ndarray_cache(f"{artifact_path}/params", device)
+    for model in ["rrdb"]:
+        plist = []
+        size = meta[f"{model}ParamSize"]
+        for i in range(size):
+            plist.append(params[f"{model}_{i}"])
+        pdict[model] = plist
+    return pdict
+
+const_params_dict = load_params(artifact_path="/Users/guoyaoli/tvm_work/web-real-esrgan/dist", device=device)
+ex = tvm.runtime.load_module("/Users/guoyaoli/tvm_work/web-real-esrgan/dist/real_esrgan.so")
+
+vm = relax.VirtualMachine(rt_mod=ex, device=device)
+
+class TVMESRPipeline:
+    def __init__(
+        self,
+        vm: relax.VirtualMachine,
+        tvm_device,
+        param_dict,
+    ):
+        def wrapper(f, params):
+            def wrapped_f(*args):
+                return f(*args, params)
+
+            return wrapped_f
+
+        self.vm = vm
+        
+        self.rrdb = wrapper(vm["rrdb"], param_dict["rrdb"])
+        
+        self.scale_image = vm["scale_image"]
+        self.preprocess = vm["preprocess"]
+        self.unscale_image = vm["unscale_image"]
+        self.postprocess = vm["postprocess"]
+
+        self.tvm_device = tvm_device
+        self.param_dict = param_dict
+
+    def __call__(self, input_image: np.array):
+        image = tvm.nd.array(input_image, device=self.tvm_device)
+        image = self.scale_image(image)
+        image = self.preprocess(image)
+        image = self.rrdb(image)
+        image = self.postprocess(image)
+        image = self.unscale_image(image)
+
+        return image
+    
+
+pipe = TVMESRPipeline(vm, device, const_params_dict)
+
+input_path = "/Users/guoyaoli/tvm_work/web-real-esrgan/input/OST_009.png"
+output_path = "./output"
+
+imgname, extension = os.path.splitext(os.path.basename(input_path))
+img = cv2.imread(input_path, cv2.IMREAD_UNCHANGED)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+img = img.astype(np.float32)
+
+output = pipe(img)
+
+final_result = output.numpy().astype(np.uint8)
+cv2.imwrite("./output/TVM_output.png", final_result)
