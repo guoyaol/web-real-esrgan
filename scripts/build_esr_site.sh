8a8e295ade9378aa0ba68bde0026186df223ebd2
guoyaol <guoyaoli00@gmail.com>
2023-06-19 16:02:12 -0700

    can show rgb to bgr

--- scripts/build_esr_site.sh
+++ scripts/build_esr_site.sh
@@ -12,6 +12,7 @@ echo "Copy local configurations.."
 cp $1 site/real-esrgan-config.json
 echo "Copy files..."
 cp web/real_esrgan.html site/_includes
+cp web/ref.html site/_includes
 cp web/real_esrgan.js site/dist
 cp web/script.js    site/dist
 
--- web/script.js
+++ web/script.js
@@ -1,44 +1,29 @@
-let imageProcessed = false; // flag to check if image has been processed
+window.onload = function() {
+    const imageUpload = document.getElementById('imageUpload');
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
 
-document.getElementById('imgInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-
-    if (file) {
+    imageUpload.addEventListener('change', function() {
         const reader = new FileReader();
-
-        reader.onload = function(e) {
-            const imgDisplay = document.getElementById('imgDisplay');
-            imgDisplay.style.display = 'block';
-            imgDisplay.src = e.target.result;
-
-            imgDisplay.onload = function() {
-                if (imageProcessed) return; // skip if image is already processed
-
-                const canvas = document.getElementById('canvas');
-                const context = canvas.getContext('2d');
-
-                canvas.width = imgDisplay.width;
-                canvas.height = imgDisplay.height;
-
-                context.drawImage(imgDisplay, 0, 0, imgDisplay.width, imgDisplay.height);
-                const imageData = context.getImageData(0, 0, imgDisplay.width, imgDisplay.height);
-
-                for(let i = 0; i < imageData.data.length; i += 4) {
-                    const r = imageData.data[i];
-                    const b = imageData.data[i+2];
-
-                    // swap red and blue channel
-                    imageData.data[i] = b;
-                    imageData.data[i+2] = r;
+        reader.onload = function(event) {
+            const img = new Image();
+            img.onload = function() {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                context.drawImage(img, 0, 0, img.width, img.height);
+                let imageData = context.getImageData(0, 0, img.width, img.height);
+                console.log(imageData);
+                for (let i = 0; i < imageData.data.length; i += 4) {
+                    let red = imageData.data[i];
+                    let blue = imageData.data[i + 2];
+                    // swap red and blue
+                    imageData.data[i] = blue;
+                    imageData.data[i + 2] = red;
                 }
-
                 context.putImageData(imageData, 0, 0);
-                imgDisplay.src = canvas.toDataURL();
-
-                imageProcessed = true; // set flag to true after processing image
-            }
-        }
-
-        reader.readAsDataURL(file);
-    }
-});
+            };
+            img.src = event.target.result;
+        };
+        reader.readAsDataURL(this.files[0]);
+    }, false);
+};
