9b01a37ba208c1e3d84df644b5e7029f4e78ab0e
guoyaol <guoyaoli00@gmail.com>
2023-06-19 03:38:27 -0400

    correct path

--- tuning/tune_small.py
+++ tuning/tune_small.py
@@ -2,7 +2,7 @@ import pickle
 from torch.nn import functional as F
 import tvm
 
-with open('deploy_mod.pkl', 'rb') as f:
+with open('/Users/guoyaol/web-real-esrgan/deploy_mod_small.pkl', 'rb') as f:
     mod_deploy = pickle.load(f)
 
 def tune(mod: tvm.IRModule) -> None:
@@ -23,9 +23,9 @@ def tune(mod: tvm.IRModule) -> None:
                 session_timeout_sec=50,
             )
         ),
-        work_dir="log_db_tuning",
-        max_trials_global=25000,
-        max_trials_per_task=1000,
+        work_dir="log_db_tuning_5_small",
+        max_trials_global=125,
+        max_trials_per_task=5,
         strategy=ms.search_strategy.EvolutionarySearch(init_min_unmeasured=10, max_fail_count=20),
     )
 
