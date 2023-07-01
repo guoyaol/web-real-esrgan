f30d7ec8ef8f1d771263890613b7ba394c8d5b6c
guoyaol <guoyaoli00@gmail.com>
2023-07-01 16:04:52 -0700

    add file for user

---
+++ my-package/user.js
@@ -0,0 +1,16 @@
+//TODO: how to imoport from src/RealEsrgan.js?
+  
+localRealESRGANInst = new RealESRGANInstance();
+
+tvmjsGlobalEnv.loadImage = function (event) {
+localRealESRGANInst.loadImage(event);
+};
+
+tvmjsGlobalEnv.asyncOnGenerate = async function () {
+await localRealESRGANInst.generate();
+};
+
+tvmjsGlobalEnv.asyncOnRPCServerLoad = async function (tvm) {
+const inst = new RealESRGANInstance();
+await inst.asyncInitOnRPCServerLoad(tvm);
+};
